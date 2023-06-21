7558f175cbbb6ab90962c32d22d6f8c0983bb19c
amirul <amirul281000@gmail.com>
2023-06-22 01:35:52 +0700

    update-with-ORM

--- repositories/users.go
+++ repositories/users.go
@@ -9,6 +9,8 @@ import (
 type UserRepository interface {
 	FindUsers() ([]models.User, error)
 	GetUser(ID int) (models.User, error)
+	CreateUser(user models.User) (models.User, error)
+	UpdateUser(user models.User) (models.User, error)
 }
 
 type repository struct {
@@ -21,14 +23,26 @@ func RepositoryUser(db *gorm.DB) *repository {
 
 func (r *repository) FindUsers() ([]models.User, error) {
 	var users []models.User
-	// Using "Find" method here ...
+	err := r.db.Find(&users).Error
 
 	return users, err
 }
 
 func (r *repository) GetUser(ID int) (models.User, error) {
 	var user models.User
-	// Using "First" method here ...
+	err := r.db.First(&user, ID).Error
+
+	return user, err
+}
+
+func (r *repository) CreateUser(user models.User) (models.User, error) {
+	err := r.db.Create(&user).Error
+
+	return user, err
+}
+
+func (r *repository) UpdateUser(user models.User) (models.User, error) {
+	// Using "Save" method here ...
 
 	return user, err
 }
